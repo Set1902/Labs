eb9bf6358dbd25cd7d9e17fba3d75866dcdcac28
set27 <set27@SERGEIKOVALAFD5>
2019-12-18 13:06:57 +0300

    релиз

---
+++ Lab 4/Files.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Files
+{
+    public partial class Files : Form
+    {
+        public Files()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Text files|*.txt";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                string text = File.ReadAllText(fd.FileName);
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+
+                    if (!list.Contains(str)) list.Add(str);
+                }
+
+                t.Stop();
+                this.textBox1ReadTime.Text = t.Elapsed.ToString();
+                this.textBox1FileReadCount.Text = list.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You must select a file");
+            }
+
+        }
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSaveReport_Click(object sender, EventArgs e)
+        {
+            string TempReportFileName = "Report_" + DateTime.Now.ToString("yyyy_MM_dd_hhmmss");
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.FileName = TempReportFileName;
+            fd.DefaultExt = ".html";
+            fd.Filter = "HTML Reports|*.html";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string ReportFileName = fd.FileName;
+                StringBuilder b = new StringBuilder();
+                b.AppendLine("<html>");
+                b.AppendLine("<head>");
+                b.AppendLine("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
+                b.AppendLine("<title>" + "Report: " + ReportFileName + "</title>");
+                b.AppendLine("</head>");
+                b.AppendLine("<body>");
+                b.AppendLine("<h1>" + "Report: " + ReportFileName + "</h1>");
+                b.AppendLine("<table border='1'>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Reading time from file</td>");
+                b.AppendLine("<td>" + this.textBox1ReadTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>The number of unique words in the file</td>");
+                b.AppendLine("<td>" + this.textBox1FileReadCount.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Key word</td>");
+                b.AppendLine("<td>" + this.textBoxFind.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Maximum distance for fuzzy search</td>");
+                b.AppendLine("<td>" + this.textBoxMaxDist.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Clear search time</td>");
+                b.AppendLine("<td>" + this.textBoxExactTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Clear Search Time</td>");
+                b.AppendLine("<td>" + this.textBoxApproxTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr valign='top'>");
+                b.AppendLine("<td>Searching results</td>");
+                b.AppendLine("<td>");
+                b.AppendLine("<ul>");
+                foreach (var x in this.listBoxResult.Items)
+                {
+                    b.AppendLine("<li>" + x.ToString() + "</li>");
+                }
+                b.AppendLine("</ul>");
+                b.AppendLine("</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("</table>");
+                b.AppendLine("</body>");
+                b.AppendLine("</html>");
+                File.AppendAllText(ReportFileName, b.ToString());
+                MessageBox.Show("The report is generated. File: " + ReportFileName);
+            }
+        }
+
+        private void buttonExact_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("You must select a file and enter a word to search");
+            }
+
+        }
+
+        private void buttonApprox_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                int maxDist;
+                if (!int.TryParse(this.textBoxMaxDist.Text.Trim(), out maxDist))
+                {
+                    MessageBox.Show("Maximum distance required");
+                    return;
+                }
+                if (maxDist < 1 || maxDist > 5)
+                {
+                    MessageBox.Show("The maximum distance must be between 1 and 5");
+                    return;
+                }
+                string wordUpper = word.ToUpper();
+                List<Tuple<string, int>> tempList = new List<Tuple<string, int>>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    int dist = EditDistance.Distance(str.ToUpper(), wordUpper);
+                    if (dist <= maxDist)
+                    {
+                        tempList.Add(new Tuple<string, int>(str, dist));
+                    }
+                }
+                t.Stop();
+                this.textBoxApproxTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (var x in tempList)
+                {
+                    string temp = x.Item1 + "(distance =" + x.Item2.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("You must select a file and enter a word to search");
+            }
+        }
+
+        private void Files_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
