8d1e527e3cec9daca18f6f6443e8580918900380
Set1902 <55362948+Set1902@users.noreply.github.com>
2020-12-28 20:39:57 +0300

    Lab2
    Lab2

---
+++ Лабораторная 2/Lab2/PythonApplication2/PythonApplication2.py
@@ -0,0 +1,20 @@
+from lab_python_oop.Rectangle import Rectangle
+from lab_python_oop.Circle import Circle
+from lab_python_oop.Square import Square
+
+
+
+def main():
+
+
+    rectangle = Rectangle("синего", 2, 2)
+    circle = Circle("зеленого", 5)
+    square = Square("красного", 2)
+
+    print(rectangle)
+    print(circle)
+    print(square)
+
+
+
+main()
